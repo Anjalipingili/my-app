5b156bcaf807e73f01ccb58302f8611b6d1c4546
anjali <anjalireddy937@gmail.com>
2024-12-21 10:34:40 +0530

    adding all files together

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Orange from './Orange';
+import Apple from './Apple';
+function App() {
+ return(
+ <div className="App">
+<Apple name="Anjali"/>
+<orange/>
+</div>
+ );
+
+}
+export default Apple;
